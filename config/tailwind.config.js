a18822981181644adb5ca17a54918f6bfe5823a8
Personal <Personal@MacBook-Pro-de-Juan-2.local>
2023-05-20 14:20:01 -0500

    Adds tailwin more configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,16 +7,7 @@ module.exports = {
     './app/views/**/*',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+  plugins: []
+  
 }
