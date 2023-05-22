337c71a6017ed8085b95298cdeceb89b31483139
Personal <Personal@MacBook-Pro-de-Juan-2.local>
2023-05-22 00:37:31 -0500

    Adds name card function

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -13,7 +13,7 @@ class CreditcardsController < ApplicationController
 		validator = Creditcard.validate_credit_card(number_card: credit_card_params[:number_credit])
 		num = credit_card_params[:number_credit]
 		if validator.present?
-			credit_card = Creditcard.new(name_card: credit_card_params[:credit_card_params], number_card: num, exp_date: credit_card_params[:exp_date].to_date, user_id: current_user.id, company_credit_card_id: validator)
+			credit_card = Creditcard.new(name_card: credit_card_params[:name_card], number_card: num, exp_date: credit_card_params[:exp_date].to_date, user_id: current_user.id, company_credit_card_id: validator)
 			if credit_card.save
 				redirect_to root_path 
 			end
