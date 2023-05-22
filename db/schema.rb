b4844b50301b213ccc08a968c038049b25cfc6ba
Personal <Personal@MacBook-Pro-de-Juan-2.local>
2023-05-22 16:06:06 -0500

    Adds admin index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_030943) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_054541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_030943) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "company_credit_card_id", null: false
+    t.string "alias"
     t.index ["company_credit_card_id"], name: "index_creditcards_on_company_credit_card_id"
     t.index ["user_id"], name: "index_creditcards_on_user_id"
   end
