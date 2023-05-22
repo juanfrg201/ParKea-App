b3a37c590d8f9e337358d556217ebebf5e5dcf59
Personal <Personal@MacBook-Pro-de-Juan-2.local>
2023-05-21 20:43:25 -0500

    Removes and modifies tables 2

--- db/migrate/20230522013514_modifies_tables.rb
+++ db/migrate/20230522013514_modifies_tables.rb
@@ -1,8 +1,7 @@
 class ModifiesTables < ActiveRecord::Migration[7.0]
   def up
-    
-    drop_table(:roles, if_exists: true)
-    drop_table(:user_roles, if_exists: true)
+    drop_table :roles, if_exists: true
+    drop_table :user_roles, if_exists: true
     remove_column :users, :roles_id, if_exists: true
     remove_column :users, :user_role_id, if_exists: true
   end
